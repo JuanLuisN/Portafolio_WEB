60a93fe7929155347f16ae588169e8dd1831f8f5
LuisKnife <jl.noriega2000@gmail.com>
2022-02-16 13:03:40 -0600

    Se termino el modulo proyectos. Ahora se pueden agregar, editar y eliminar proyectos

---
+++ src/controllers/systemControllers/admin/proyectos.controller.js
@@ -0,0 +1,68 @@
+const connection = require('../../../database')
+
+const redirectPath = '/proyectos'
+
+controller = {}
+
+controller.renderProyectos = async (req, res) => {
+    const proyectos = await connection.query('select * from proyectos order by id DESC')
+    res.render('system/admin/adminProyectos', {
+        proyectos
+    })
+}
+
+controller.saveProyecto = async (req, res) => {
+    const { imagen, proyecto, fechaInicial, fechaFinal, descripcion, repositorio } = req.body
+    const newProyecto = {
+        imagen,
+        proyecto,
+        fecha: fechaInicial + ' | ' + fechaFinal,
+        descripcion,
+        repositorio
+    }
+    try {
+
+        await connection.query('insert into proyectos set ?', [newProyecto])
+        req.flash('success_msg', 'Proyecto agregado correctamente')
+        res.redirect(redirectPath)
+    } catch (error) {
+        console.log(error)
+        req.flash('error_msg', 'Algo salio mal. Intentalo de nuevo')
+    }
+}
+
+controller.editProyecto = async (req, res) => {
+    const { id } = req.params
+    const { imagen, proyecto, fechaInicial, fechaFinal, descripcion, repositorio } = req.body
+    const newProyecto = {
+        imagen,
+        proyecto,
+        fecha: fechaInicial + ' | ' + fechaFinal,
+        descripcion,
+        repositorio
+    }
+    try {
+        await connection.query('update proyectos set imagen = ?, proyecto = ?, fecha = ?, descripcion = ?, repositorio = ? where id = ?', 
+            [newProyecto.imagen, newProyecto.proyecto, newProyecto.fecha, newProyecto.descripcion, newProyecto.repositorio, id])
+        req.flash('success_msg', 'Se actualizo el proyecto con exito')
+        res.redirect(redirectPath)
+    } catch (error) {
+        console.log(error)
+        req.flash('error_msg', 'Algo salio mal. Intentalo de nuevo')
+    }
+}
+
+controller.deleteProyecto = async (req, res) => {
+    const { id } = req.params
+    try {
+        await connection.query('delete from proyectos where id = ?', [id])
+        req.flash('success_msg', 'Se elimino el proyecto con exito')
+        res.redirect(redirectPath)
+    } catch (error) {
+        console.log(error)
+        res.flash('error_msg', 'Algo salio mal. Intentalo de nuevo')
+    }
+}
+
+
+module.exports = controller
--- src/routes/systemRoutes/admin/admin.routes.js
+++ src/routes/systemRoutes/admin/admin.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const descripcionesController = require('../../../controllers/systemControllers/admin/descripciones.controller')
 const habilidadesController = require('../../../controllers/systemControllers/admin/habilidades.controller')
+const proyectosController = require('../../../controllers/systemControllers/admin/proyectos.controller')
 
 router.get('/descripciones', descripcionesController.renderDescripciones)
 router.post('/descripciones/add', descripcionesController.saveDescripcion)
@@ -14,5 +15,9 @@ router.post('/habilidades/add', habilidadesController.saveHabilidad)
 router.post('/habilidades/edit/:id', habilidadesController.editHabilidad)
 router.get('/habilidades/delete/:id', habilidadesController.deleteHabilidad)
 
+router.get('/proyectos', proyectosController.renderProyectos)
+router.post('/proyectos/add', proyectosController.saveProyecto)
+router.post('/proyectos/edit/:id', proyectosController.editProyecto)
+router.get('/proyectos/delete/:id', proyectosController.deleteProyecto)
 
 module.exports = router
